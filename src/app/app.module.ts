8bbc7e51a30c4c7cfdfa3afb8621e24d1ff9d1f1
brian.musselman <BrianMusselman89@gmail.com>
2018-07-05 14:36:06 -0400

    added create account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule
