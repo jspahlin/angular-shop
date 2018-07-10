e2e5804f593f03a1acb1af95b103f80b3b5e172a
zia mohiuddin <zain-sheikh@live.com>
2018-07-09 20:36:58 -0400

    more stuff

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { CartComponent } from'src/app/cart/cart.component';
 import { LoginComponent } from 'src/app/login/login.component';
 import { HomeComponent } from 'src/app/home/home.component';
 <<<<<<< HEAD
 import { CreateAccountComponent } from '../create-account/create-account.component';
 =======
+import {AddCardComponent} from '../add-card/add-card.component';
+import {CardRaritySelectorComponent} from '../card-rarity-selector/card-rarity-selector.component';
 >>>>>>> 5dad2f03de52d59150151e07bb15658a83a920b9
 
 const routes = [
@@ -27,6 +30,18 @@ const routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'add-card',
+    component: AddCardComponent
+  },
+  {
+    path: 'rarity',
+    component: CardRaritySelectorComponent
   }
 ];
 @NgModule({
